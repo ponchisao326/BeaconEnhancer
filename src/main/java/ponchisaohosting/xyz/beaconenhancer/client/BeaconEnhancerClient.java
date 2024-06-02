8aaa11380157eceaf07caa4dd4144e6141d2f39b
ponchisao326_ <ponchisao326@hotmail.com>
2024-06-02 16:08:20 +0200

    Added client Logger info

--- src/main/java/ponchisaohosting/xyz/beaconenhancer/client/BeaconEnhancerClient.java
+++ src/main/java/ponchisaohosting/xyz/beaconenhancer/client/BeaconEnhancerClient.java
@@ -1,11 +1,17 @@
 package ponchisaohosting.xyz.beaconenhancer.client;
 
 import net.fabricmc.api.ClientModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class BeaconEnhancerClient implements ClientModInitializer {
 
+    public static final String MOD_ID = "beaconenhancer";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
     @Override
     public void onInitializeClient() {
-
+        LOGGER.info("Initiating BeaconEnhancer");
+        LOGGER.info("BeaconEnhancer autor: Ponchisao326");
     }
 }
